4de139d6a82d6899818cb3c2889da97f223b3a40
Hugo Araya C <42989121+hugo-araya@users.noreply.github.com>
2024-04-22 09:48:42 -0400

    Create interes.py

---
+++ interes.py
@@ -0,0 +1,6 @@
+# Entero cap_invertir
+# Real ganancia
+cap_invertir = int(input('Ingrese capital: '))
+ganancia = cap_invertir * 2 / 100
+salida = 'Para una inversion de $'+str(cap_invertir)+' la ganancia es $'+str(ganancia)
+print(salida)
